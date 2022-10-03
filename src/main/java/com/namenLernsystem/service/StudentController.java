6c78b69f74bb465151580d969f25bc0cdaf0961f
pascalyazid <pascal.thuma@gmail.com>
2022-10-03 10:54:35 +0200

    DataHandler

--- src/main/java/com/namenLernsystem/service/StudentController.java
+++ src/main/java/com/namenLernsystem/service/StudentController.java
@@ -1,15 +1,22 @@
-package com.namenLernsystem.service
+package com.namenLernsystem.service;
 
-import com.namenLernsystem.data.DataHandler
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import com.namenLernsystem.data.DataHandler;
+import com.namenLernsystem.model.Student;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
-@RequestMapping(value = "/list", method = RequestMethod.GET)
-
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    List<Student> allStudents() throws IOException {
+        return DataHandler.readJSON();
+    }
 }
