57e8eca2cffe932c4f75d32d832740d0b951c49a
pascalyazid <pascal.thuma@gmail.com>
2022-10-03 10:04:31 +0200

    initial commit

---
+++ src/main/java/com/namenLernsystem/NamenLernsystemApplication.java
@@ -0,0 +1,13 @@
+package com.namenLernsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamenLernsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamenLernsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/namenLernsystem/NamenLernsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namenLernsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamenLernsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
