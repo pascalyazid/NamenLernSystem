c7a9dec2e3dcfa3dbde297e0f12b480f12c41d31
Hermann Witte <hermann.witte@alpha-lab.net>
2022-10-03 11:46:48 +0200

    DataHandler readFiles

--- src/main/java/com/namenLernsystem/model/Student.java
+++ src/main/java/com/namenLernsystem/model/Student.java
@@ -1,4 +1,67 @@
 package com.namenLernsystem.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+/**
+ * @author Hermann Witte
+ * @since 3.10.2022
+ * @version 1.0
+ * */
+@Entity
 public class Student {
+    @Id
+    private String id = UUID.randomUUID().toString();
+    private String firstName;
+    private String lastName;
+    private String className;
+    private String path;
+
+    public Student(String firstName, String lastName, String className, String path) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.className = className;
+        this.path = path;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
--- src/test/java/com/namenLernsystem/NamenLernsystemApplicationTests.java
+++ src/test/java/com/namenLernsystem/NamenLernsystemApplicationTests.java
@@ -1,6 +1,6 @@
 package com.namenLernsystem;
 
-import org.testng.annotations.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
